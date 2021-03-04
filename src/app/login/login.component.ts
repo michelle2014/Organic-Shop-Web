24ec1a71f63cd755f2953b3897e15cf3e06432b0
michelle <michelle.transbetter@gmail.com>
2021-03-04 18:29:27 +1000

    firebase authentication

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import firebase from 'firebase/app';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private afAuth: AngularFireAuth ) { }
+
+  ngOnInit(): void {
+  }
+
+  login() {
+    this.afAuth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+  }
+
+}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAKjnf0kBFBjniWLzcbt3l1VlPs3XqX3-E",
+    authDomain: "organic-shop-a4bbb.firebaseapp.com",
+    projectId: "organic-shop-a4bbb",
+    storageBucket: "organic-shop-a4bbb.appspot.com",
+    messagingSenderId: "552210580711",
+    appId: "1:552210580711:web:6a82721f70ed7cde9ece42"
+  }
 };
